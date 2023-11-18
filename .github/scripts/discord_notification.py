9a1ee105ebc1f53dca415d6c86e98dadc4ff8993
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-18 14:34:39 -0600

    Update discord_notification.py

--- .github/scripts/discord_notification.py
+++ .github/scripts/discord_notification.py
@@ -1,13 +1,11 @@
 import os
 import base64
-from discord import Webhook, RequestsWebhookAdapter, Embed, Color
+import requests
+from discord import Embed, Color
 
 # Decode Base64-encoded token
 base64_encoded_token = os.getenv('TVRFM05UVXlNRGN3TmpBNU5qZ3hNakU0TXcuR0tnb2FNLjNsTTNnRmIyWTZkQVBRa1hDQ2NmZU12TXYwY25JXzdENUVOc0Jn')
 discord_bot_token = base64.b64decode(base64_encoded_token).decode('utf-8')
-
-# Discord Webhook Setup using Bot Token
-discord_webhook = Webhook.from_bot_token(discord_bot_token, adapter=RequestsWebhookAdapter())
 
 # Example Discord notification
 embed = Embed(
@@ -16,4 +14,15 @@ embed = Embed(
     color=Color.green()
 )
 
-discord_webhook.send(embed=embed)
+# Set up the Discord Webhook URL
+discord_webhook_url = f'https://discord.com/api/webhooks/{discord_bot_token}'
+
+# Send the webhook using the requests library
+response = requests.post(discord_webhook_url, json={'embeds': [embed.to_dict()]})
+
+# Check if the request was successful
+if response.status_code == 204:
+    print('Discord webhook sent successfully!')
+else:
+    print(f'Error sending Discord webhook. Status code: {response.status_code}')
+    print(response.text)
