1b71b054a244abcb0628b3e335ae67aeb72f4679
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2023-11-18 15:57:54 -0600

    Update discord_notification.py

--- .github/scripts/discord_notification.py
+++ .github/scripts/discord_notification.py
@@ -1,18 +1,30 @@
 import os
-from discord import Webhook, RequestsWebhookAdapter, Embed, Color
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Discord Webhook Setup
 discord_webhook_url = os.getenv('https://discord.com/api/webhooks/1175551243414949998/PRGTbBzRiIICLvtLduuhnhIxhmjI34F2n7eY3a9IdYQik5vN44HXzzb-PcgJkGSPMCE-')
-discord_webhook = Webhook.from_url(discord_webhook_url, adapter=RequestsWebhookAdapter())
 
 # Example Discord notification
-embed = Embed(
-    title='GitHub Action Triggered!',
-    description='There has been a new push to the repository.',
-    color=Color.green()
-)
+payload = {
+    'content': 'GitHub Action Triggered!',
+    'embeds': [
+        {
+            'title': 'There has been a new push to the repository.',
+            'color': 65280  # Green color
+        }
+    ]
+}
 
-discord_webhook.send(embed=embed)
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(discord_webhook_url, json=payload, headers=headers)
+
+if response.status_code == 200:
+    print('Notification sent successfully!')
+else:
+    print(f'Error sending notification. Status code: {response.status_code}, Response: {response.text}')
