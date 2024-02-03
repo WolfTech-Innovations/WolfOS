c69e43a743d98ac384c7915cbef2a04b594f1909
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-02-02 19:32:19 -0600

    Update discord_notification.py

--- .github/scripts/discord_notification.py
+++ .github/scripts/discord_notification.py
@@ -4,30 +4,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Discord Webhook Setup
-discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+def send_discord_notification(message, color=65280):
+    discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 
-if discord_webhook_url is None:
-    raise ValueError('Discord webhook URL is missing. Make sure to set DISCORD_WEBHOOK_URL in your .env file.')
+    if discord_webhook_url is None:
+        raise ValueError('Discord webhook URL is missing. Make sure to set DISCORD_WEBHOOK_URL in your .env file.')
 
-# Example Discord notification
-payload = {
-    'content': 'GitHub Action Triggered!',
-    'embeds': [
-        {
-            'title': 'There has been a new push to the repository.',
-            'color': 65280  # Green color
-        }
-    ]
-}
+    payload = {
+        'content': message,
+        'embeds': [
+            {
+                'title': message,
+                'color': color
+            }
+        ]
+    }
 
-headers = {
-    'Content-Type': 'application/json'
-}
+    headers = {
+        'Content-Type': 'application/json'
+    }
 
-response = requests.post(discord_webhook_url, json=payload, headers=headers)
+    response = requests.post(discord_webhook_url, json=payload, headers=headers)
 
-if response.status_code == 200:
-    print('Notification sent successfully!')
-else:
-    print(f'Error sending notification. Status code: {response.status_code}, Response: {response.text}')
+    if response.status_code == 200:
+        print('Notification sent successfully!')
+    else:
+        print(f'Error sending notification. Status code: {response.status_code}, Response: {response.text}')
+
+if __name__ == "__main__":
+    # Example Discord notification
+    send_discord_notification('GitHub Action Triggered!')
